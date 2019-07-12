87465b766ee7aac2fc36e6c90370258f815d0021
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-07-12 11:04:40 -0700

    Sprint Challenge

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,19 +5,63 @@ from hashtables import (HashTable,
                         hash_table_retrieve,
                         hash_table_resize)
 
+# #Brute Force
+# def get_indices_of_item_weights(weights, length, limit):
+#     ht = HashTable(16)
+
+#     """
+#     YOUR CODE HERE
+#     """
+#     if len(weights) is 1:
+#         return None
+#     winners = []
+#     for i in range(len(weights)):
+#         for j in range(i+1, len(weights)):
+#             if weights[i] + weights[j] == limit:
+#                 if j > i:
+#                     good = [j,i]
+#                 else:
+#                     good = [i,j]
+                
+#                 # print(good)
+#                 winners.append(good)
+
+#     best_winner_index = 0
+#     best_winner_number = 0
+#     for i in range(len(winners)):
+#         if weights[winners[i][0]] > best_winner_number:
+#             best_winner_number = weights[winners[i][0]]
+#             best_winner_index = i
+
+#     return winners[best_winner_index]
+
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
 
     """
     YOUR CODE HERE
     """
+    if len(weights) == 1:
+        return None
+    ht = HashTable(16)
+    winner = []
+    for i in range(len(weights)):
+        hash_table_insert(ht, weights[i], i)
 
+    for i in weights:
+        search = limit - i
+        search_ht = hash_table_retrieve(ht, search)
+        if search_ht:
+            # print("GOOD", search_ht)
+            winner.append(search_ht)
+            hash_table_remove(ht, search)
+            winner.append(hash_table_retrieve(ht, i))
+            return winner
     return None
 
 
 def print_answer(answer):
-    if answer is None:
+    if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
